8e411cfc511d11e04d30b489aaa27da55329b349
Luiz <luizribeirocorreia09@gmail.com>
2026-03-13 15:06:13 -0300

    Add three buttons for playing Rock-Paper-Scissors game
    add there buttons and their correspond interectivity so user can play

--- javascript.js
+++ javascript.js
@@ -10,7 +10,7 @@ function computer_choice() {
             return "scissors";
     }
 }
-// Write a function to get what the human is going to play
+// Write a function to get what the human is going to play from console
 function human_choice() {
     let choice = prompt('Choose "ROCK", "PAPER" or "SCISSORS": ').toLowerCase();
     return choice;
@@ -19,46 +19,56 @@ function human_choice() {
 let human_scores = 0;
 let computer_scores = 0;
 // Write logic to play a single round
-function round(){
+function round(user_choice){
     let robot_choice = computer_choice();
-    let user_choice = human_choice();
 
     if (user_choice === "rock") {
         if (robot_choice === "paper") {
             console.log("You lose! paper beats rock");
             computer_scores++;
+            return "lose";
         } else if (robot_choice === "scissors") {
             console.log("You win! rock beats scissors");
             human_scores++;
+            return "win";
         } else {
             console.log("Draw!");
+            return "draw";
         }
     }
    if (user_choice === "paper") {
         if (robot_choice === "scissors") {
             console.log("You lose! scissors beats paper");
             computer_scores++;
+            return "lose";
         } else if (robot_choice === "rock") {
             console.log("You win! paper beats rock");
             human_scores++;
+            return "win";
         } else {
             console.log("Draw!");
+            return "draw";
         }
     }
     if (user_choice === "scissors") {
         if (robot_choice === "rock") {
             console.log("You lose! rock beats scissors");
             computer_scores++;
+            return "lose";
         } else if (robot_choice === "paper") {
             console.log("You win! scissors beats paper");
             human_scores++;
+            return "win";
         } else {
             console.log("Draw!");
+            return "draw";
         }
     }
 }
-// Write a function to allow play five rounds
-for (let i = 0; i < 5; i++) {
-    round();
-    alert(`Your score: ${human_scores}, computer scores: ${computer_scores}`);
+
+const buttons = document.querySelectorAll("button");
+for (const button of buttons) {
+    button.addEventListener("click", (event) => {
+        round(event.target.value);
+    })
 }
