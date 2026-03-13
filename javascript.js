7cb0f422fa703f5ab95be909d3427d7c90fde817
Luiz <luizribeirocorreia09@gmail.com>
2026-03-13 15:25:07 -0300

    Add a display for results
    use a display for showing who won the round (if any) or
    show who wins that hand

--- javascript.js
+++ javascript.js
@@ -26,49 +26,71 @@ function round(user_choice){
         if (robot_choice === "paper") {
             console.log("You lose! paper beats rock");
             computer_scores++;
-            return "lose";
+            return "lose!";
         } else if (robot_choice === "scissors") {
             console.log("You win! rock beats scissors");
             human_scores++;
-            return "win";
+            return "win!";
         } else {
             console.log("Draw!");
-            return "draw";
+            return "draw!";
         }
     }
    if (user_choice === "paper") {
         if (robot_choice === "scissors") {
             console.log("You lose! scissors beats paper");
             computer_scores++;
-            return "lose";
+            return "lose!";
         } else if (robot_choice === "rock") {
             console.log("You win! paper beats rock");
             human_scores++;
-            return "win";
+            return "win!";
         } else {
             console.log("Draw!");
-            return "draw";
+            return "draw!";
         }
     }
     if (user_choice === "scissors") {
         if (robot_choice === "rock") {
             console.log("You lose! rock beats scissors");
             computer_scores++;
-            return "lose";
+            return "lose!";
         } else if (robot_choice === "paper") {
             console.log("You win! scissors beats paper");
             human_scores++;
-            return "win";
+            return "win!";
         } else {
             console.log("Draw!");
-            return "draw";
+            return "draw!";
         }
     }
 }
+const resultsH1 = document.querySelector("#resultsH1");
+const resultsH2 = document.querySelector("#resultsH2");
+function updateScores() {
+    resultsH1.textContent = `You: ${human_scores} / Robot: ${computer_scores}`;
+}
 
+function isGameOver() {
+    if (human_scores >= 5) {
+        resultsH1.textContent = "You Won the round!"
+        human_scores = 0;
+        computer_scores = 0;
+    } else if (computer_scores >= 5) {
+        resultsH1.textContent = "Computer won the round!"
+        human_scores = 0;
+        computer_scores = 0;
+    }
+}
+
+updateScores();
 const buttons = document.querySelectorAll("button");
 for (const button of buttons) {
     button.addEventListener("click", (event) => {
-        round(event.target.value);
+        resultsH2.textContent = round(event.target.value);
+        updateScores();
+        isGameOver();
     })
 }
+
+
